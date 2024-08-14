24a279dd7978ab1c12e563752322ef0ed4a83bb9
kartik kaushik <kartik.kaushik@acceleratebs.com>
2024-08-14 14:10:06 +0530

    second

---
+++ config/app.ts
@@ -0,0 +1,5 @@
+
+// to run the file in watch mode use tsc app.ts --watch or -w
+// combining mulriple tyescrip filesm first initialize 1) tsc --init 2)use tsc to compile all ts files to js 3)just use tsc
+
+console.log("Typescript compiler configuration")
